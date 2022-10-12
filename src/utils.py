a4f004a81c1024532a8e890bbe282c26977e089a
Yana-Denisova <ladymacbeth@list.ru>
2022-10-12 21:58:25 +0300

    first review

---
+++ src/utils.py
@@ -0,0 +1,34 @@
+import logging
+
+from requests import RequestException
+
+from exceptions import ParserFindTagException
+
+
+def get_response(session, url):
+    """
+    Получение http ответа по url.
+    Логирование ошибки при загрузке страницы.
+    """
+    try:
+        response = session.get(url)
+        response.encoding = 'utf-8'
+        return response
+    except RequestException:
+        logging.exception(
+            f'Возникла ошибка при загрузке страницы {url}',
+            stack_info=True
+        )
+
+
+def find_tag(soup, tag, attrs=None):
+    """
+    Функция заменяющая .find().
+    Логирует ошибку, если тег не найден.
+    """
+    searched_tag = soup.find(tag, attrs=(attrs or {}))
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
